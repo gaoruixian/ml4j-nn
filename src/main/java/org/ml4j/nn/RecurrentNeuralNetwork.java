1bb610846d924a0a2e841eac6bf439c1e2b39f09
Michael Lavelle <michael@lavelle.name>
2015-09-15 12:28:28 +0100

    Theta optimisations

--- src/main/java/org/ml4j/nn/RecurrentNeuralNetwork.java
+++ src/main/java/org/ml4j/nn/RecurrentNeuralNetwork.java
@@ -332,6 +332,42 @@ public class RecurrentNeuralNetwork extends BaseFeedForwardNeuralNetwork<Directe
 		return newThetasVec;
 
 	}
+	
+	
+	protected Vector<DoubleMatrix> minimiseThetasForRetrainableLayers(SupervisedSequences inputOutputSequences, Vector<DoubleMatrix> initialRetrainableThetas,
+			double[] retrainableLambdas, CostFunction costFunction, int max_iter) {
+		
+		RecurrentNeuralNetwork duplicateNeuralNetwork = this.dup(false);
+
+		Vector<DoubleMatrix> newThetasVec = null;
+		for (int iteration = 0; iteration < max_iter; iteration++) {
+			
+			
+			for (int i = 1; i <= maxSequenceLength; i++) {
+				
+				Collection<SupervisedSequence> sequencesOfSpecifiedLength = inputOutputSequences.filterBySequenceLength(i);
+
+				for (SupervisedSequence sequence : sequencesOfSpecifiedLength) {
+
+					newThetasVec = duplicateNeuralNetwork.getUpdatedThetasForSequence(i, duplicateNeuralNetwork.getRetrainableThetas(), sequence,retrainableLambdas);
+					
+	
+					duplicateNeuralNetwork.updateThetasForRetrainableLayers(createDoubleMatricesFactory().create(newThetasVec), true);
+
+
+				}
+			}
+			double cost = duplicateNeuralNetwork.getCost(inputOutputSequences,retrainableLambdas,costFunction);
+			
+			System.out.print("Iteration " + iteration + " | Cost: " + cost + "\r");
+
+			
+
+		}
+
+		return newThetasVec;
+
+	}
 
 	private double getCost(SupervisedSequences sequences,double[] regularizationLambdas,CostFunction costFunction) {
 
